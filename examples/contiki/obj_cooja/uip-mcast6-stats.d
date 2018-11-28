64b762b2ca4a35217163afc3cf7bce07182c00d2
Instant Contiki <bes2583@ajou.ac.kr>
2018-11-28 07:46:55 +0100

    backup energy consumption measurement scenario for FDTLS: udp-client.bk, udp-server.bk

--- examples/contiki/obj_cooja/uip-mcast6-stats.d
+++
@@ -1,7 +0,0 @@
-obj_cooja/uip-mcast6-stats.o: \
- ../../../../core/net/ipv6/multicast/uip-mcast6-stats.c \
- ../../../../core/./net/ipv6/multicast/uip-mcast6-stats.h \
- ../../../../platform/cooja/./contiki-conf.h
- ../../../../core/net/ipv6/multicast/uip-mcast6-stats.c :
- ../../../../core/./net/ipv6/multicast/uip-mcast6-stats.h :
- ../../../../platform/cooja/./contiki-conf.h :
